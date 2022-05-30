53c06a61f6bb5982f7e6aeacf40837dcecbc773d
martina benitez <benitezmartina05@gmail.com>
2022-05-29 21:48:10 -0300

    ultimo commit

--- familiares/views.py
+++ familiares/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 from familiares.models import familiar
 # Create your views here.
 def familiares(request):
-    familiares = familiar.objects.all()
-    context={'familiares': familiares}
-    return render (request, 'familiares.html', context=context )
+    familiar_1 = familiar.objects.create(
+        nombre = 'Andrea',
+        apellido = 'Schianchi',
+        Tipo = 'Mamá',
+        edad = 53,
+        nacimiento = '1969-04-13'
+    )
+    familiar_2 = familiar.objects.create(
+        nombre = 'Malena',
+        apellido = 'Benitez',
+        Tipo = 'Hermana',
+        edad = 22,
+        nacimiento = '1999-10-19'
+    )
+    familiar_3 = familiar.objects.create(
+        nombre = 'Diego',
+        apellido = 'Benitez',
+        Tipo = 'Papá',
+        edad = 52,
+        nacimiento = '1970-07-07'
+    )
+    context={'familiar_1': familiar_1,'familiar_2': familiar_2, 'familiar_3': familiar_3}
+    return render (request, 'familiares.html', context=context)
